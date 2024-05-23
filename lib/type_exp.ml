1941db3a08c7a79b8fa33d7221d85bba65bf7de2
zhizhuoxin <shixins@umich.edu>
2024-05-23 02:12:19 -0400

    Move type exp to a separate module.

---
+++ lib/type_exp.ml
@@ -0,0 +1,238 @@
+open Single_exp
+open Pretty_print
+
+module TypeExp = struct
+  exception TypeExpError of string
+  let type_exp_error msg = raise (TypeExpError ("[Type Exp Error] " ^ msg))
+
+  type type_var_id = int
+
+  type type_bop =
+    | TypeAdd
+    | TypeSub
+    | TypeMul
+    | TypeSal
+    | TypeShr
+    | TypeSar
+    | TypeXor
+    | TypeAnd
+    | TypeOr
+    | TypeInter
+    | TypeUnion
+    | TypeDiff
+
+  type type_uop =
+    | TypeNot
+    | TypeComp
+
+  type t =
+    | TypeSingle of SingleExp.t
+    | TypeRange of SingleExp.t * bool * SingleExp.t * bool * int (* begin, inc, end, inc, step *)
+    | TypeVar of type_var_id
+    | TypeTop
+    | TypeBot
+    | TypeBExp of type_bop * t * t
+    | TypeUExp of type_uop * t
+
+  let rec cmp_type_exp (e1: t) (e2: t) : bool =
+    match e1, e2 with
+    | TypeSingle s1, TypeSingle s2 -> SingleExp.cmp_single_exp s1 s2 = 0
+    | TypeRange (l1, bl1, r1, br1, s1), TypeRange (l2, bl2, r2, br2, s2) ->
+      (SingleExp.cmp_single_exp l1 l2 = 0) && (bl1 = bl2) && (SingleExp.cmp_single_exp r1 r2 = 0) && (br1 = br2) && (s1 = s2)
+    | TypeVar v1, TypeVar v2 -> v1 = v2
+    | TypeTop, TypeTop -> true
+    | TypeBot, TypeBot -> true
+    | TypeBExp (bop1, l1, r1), TypeBExp (bop2, l2, r2) ->
+      (bop1 = bop2) && (cmp_type_exp l1 l2) && (cmp_type_exp r1 r2)
+    | TypeUExp (uop1, l1), TypeUExp (uop2, l2) ->
+      (uop1 = uop2) && (cmp_type_exp l1 l2)
+    | _ -> false
+
+  let rec eval_type_exp (e: t) : t =
+    match e with
+    | TypeBExp (tbop, e1, e2) -> 
+      let ee1 = eval_type_exp e1 in
+      let ee2 = eval_type_exp e2 in
+      let default_type = TypeBExp (tbop, ee1, ee2) in
+      begin match ee1, ee2 with
+      | TypeSingle s1, TypeSingle s2 -> 
+        begin match tbop with
+        | TypeAdd -> TypeSingle (SingleExp.eval (SingleBExp (SingleAdd, s1, s2)))
+        | TypeSub -> TypeSingle (SingleExp.eval (SingleBExp (SingleSub, s1, s2)))
+        | TypeMul -> TypeSingle (SingleExp.eval (SingleBExp (SingleMul, s1, s2)))
+        | TypeSal -> TypeSingle (SingleExp.eval (SingleBExp (SingleSal, s1, s2)))
+        | TypeSar -> TypeSingle (SingleExp.eval (SingleBExp (SingleSal, s1, s2)))
+        | TypeXor -> TypeSingle (SingleExp.eval (SingleBExp (SingleXor, s1, s2)))
+        | TypeAnd -> TypeSingle (SingleExp.eval (SingleBExp (SingleAnd, s1, s2)))
+        | TypeOr -> TypeSingle (SingleExp.eval (SingleBExp (SingleOr, s1, s2)))
+        | TypeInter -> 
+          begin match s1, s2 with
+          | SingleConst v1, SingleConst v2 -> if v1 = v2 then TypeSingle (SingleConst v1) else TypeBot
+          | SingleVar v1, SingleVar v2 -> if v1 = v2 then TypeSingle (SingleVar v1) else e (* TODO Double check this! *)
+          | _ -> default_type
+          end
+        | TypeDiff -> 
+          begin match s1, s2 with
+          | SingleConst v1, SingleConst v2 -> if v1 = v2 then TypeBot else TypeSingle (SingleConst v1)
+          | SingleVar v1, SingleVar v2 -> if v1 = v2 then TypeBot else TypeSingle (SingleVar v1)
+          | _ -> default_type
+          end
+        | _ -> default_type
+        end
+      | TypeSingle s0, TypeRange (s1, b1, s2, b2, step) ->
+        begin match tbop with
+        | TypeAdd -> TypeRange ((SingleExp.eval (SingleBExp (SingleAdd, s0, s1))), b1,
+                                (SingleExp.eval (SingleBExp (SingleAdd, s0, s2))), b2, step)
+        | TypeSub -> TypeRange ((SingleExp.eval (SingleBExp (SingleSub, s0, s2))), b2,
+                                (SingleExp.eval (SingleBExp (SingleSub, s0, s1))), b1, step)
+        | TypeMul -> 
+          begin match s0 with
+          | SingleConst i0 -> 
+            if i0 = 0 then TypeSingle (SingleConst 0)
+            else if i0 > 0 then
+              TypeRange ((SingleExp.eval (SingleBExp (SingleMul, s0, s1))), b1,
+                        (SingleExp.eval (SingleBExp (SingleMul, s0, s2))), b2, step * i0)
+            else
+              TypeRange ((SingleExp.eval (SingleBExp (SingleMul, s0, s2))), b2, 
+                        (SingleExp.eval (SingleBExp (SingleMul, s0, s1))), b1, - step * i0)
+          | _ -> default_type
+            (* TypeRange ((eval (SingleBExp (SingleMul, s1, s0))), b1,
+                            (eval (SingleBExp (SingleMul, s2, s0))), b2, step) *)
+          end
+        (* TODO: TypeXor TypeAnd TypeOr *)
+        | _ -> default_type
+        end
+      | TypeRange (s1, b1, s2, b2, step), TypeSingle s0 -> 
+        begin match tbop with
+        | TypeAdd -> TypeRange ((SingleExp.eval (SingleBExp (SingleAdd, s0, s1))), b1,
+                                (SingleExp.eval (SingleBExp (SingleAdd, s0, s2))), b2, step)
+        | TypeSub -> TypeRange ((SingleExp.eval (SingleBExp (SingleSub, s1, s0))), b1,
+                                (SingleExp.eval (SingleBExp (SingleSub, s2, s0))), b2, step)
+        | TypeMul -> 
+          begin match s0 with
+          | SingleConst i0 -> 
+            if i0 = 0 then TypeSingle (SingleConst 0)
+            else if i0 > 0 then
+              TypeRange ((SingleExp.eval (SingleBExp (SingleMul, s0, s1))), b1,
+                        (SingleExp.eval (SingleBExp (SingleMul, s0, s2))), b2, step * i0)
+            else
+              TypeRange ((SingleExp.eval (SingleBExp (SingleMul, s0, s2))), b2, 
+                        (SingleExp.eval (SingleBExp (SingleMul, s0, s1))), b1, - step * i0)
+          | _ -> default_type
+            (* TypeRange ((eval (SingleBExp (SingleMul, s1, s0))), b1,
+                            (eval (SingleBExp (SingleMul, s2, s0))), b2, step) *)
+          end
+        | TypeSal ->
+          begin match s0 with
+          | SingleConst i0 -> TypeRange ((SingleExp.eval (SingleBExp (SingleSal, s1, s0))), b1,
+                                         (SingleExp.eval (SingleBExp (SingleSal, s2, s0))), b2, Int.shift_left step i0)
+          | _ -> default_type
+          end
+        | TypeSar ->
+          begin match s0 with
+          | SingleConst i0 -> TypeRange ((SingleExp.eval (SingleBExp (SingleSar, s1, s0))), b1,
+                                          (SingleExp.eval (SingleBExp (SingleSar, s2, s0))), b2, Int.shift_right step i0)
+          | _ -> default_type
+          end
+        (* TODO: TypeXor TypeAnd TypeOr *)
+        | _ -> default_type
+        end
+      | TypeRange (s1, true, s2, true, step), TypeRange (s1', true, s2', true, step') ->
+        begin match tbop with
+        | TypeAdd -> 
+          TypeRange (
+            SingleExp.eval (SingleBExp (SingleAdd, s1, s1')), true,
+            SingleExp.eval (SingleBExp (SingleAdd, s2, s2')), true,
+            if step mod step' = 0 then step' 
+            else if step' mod step = 0 then step 
+            else type_exp_error ("steps not divide by each other " ^ (string_of_int step) ^ " " ^ (string_of_int step))
+          )
+        | _ -> default_type
+        end
+      | TypeTop, _ ->
+        begin match tbop with
+        | TypeInter -> ee2
+        | TypeDiff -> TypeUExp (TypeComp, ee2)
+        | _ -> TypeTop
+        end
+      | _, TypeTop ->
+        begin match tbop with
+        | TypeInter -> ee1
+        | TypeDiff -> TypeBot
+        | _ -> TypeTop
+        end
+      | _ -> default_type
+      end
+    | TypeUExp (tuop, e) ->
+      let ee = eval_type_exp e in
+      let default_type = TypeUExp (tuop, ee) in
+      begin match ee with
+      | TypeSingle s ->
+        begin match tuop with
+        | TypeNot -> TypeSingle (SingleExp.eval (SingleUExp (SingleNot, s)))
+        | _ -> default_type
+        end
+      | _ -> default_type
+      end
+    | _ -> e
+
+  let merge_type_exp (e1: t) (e2: t) : t option =
+    match e1, e2 with
+    | TypeSingle s, TypeRange (l, lb, r, rb, step)
+    | TypeRange (l, lb, r, rb, step), TypeSingle s ->
+      if lb && (SingleExp.cmp_single_exp (SingleExp.eval (SingleBExp (SingleAdd, s, SingleConst step))) l = 0) then
+        Some (TypeRange (s, lb, r, rb, step))
+      else if rb && (SingleExp.cmp_single_exp (SingleExp.eval (SingleBExp (SingleAdd, r, SingleConst step))) s = 0) then
+        Some (TypeRange (l, lb, s, rb, step))
+      else if SingleExp.cmp_single_exp s l = 0 then
+        Some (TypeRange (l, true, r, rb, step))
+      else if SingleExp.cmp_single_exp r s = 0 then
+        Some (TypeRange (l, lb, r, true, step))
+      else None
+    | _ -> None
+  
+  let is_type_exp_val (e: t) : bool =
+    match e with
+    | TypeSingle _
+    | TypeRange _
+    | TypeTop
+    | TypeBot -> true
+    | _ -> false
+
+  let rec string_of_type_exp (t: t) =
+    match t with
+    | TypeSingle ts -> SingleExp.string_of_single_exp ts
+    | TypeRange (bg, bgi, ed, edi, step) -> 
+      let bg_str = if bgi then "[" else "(" in
+      let ed_str = if edi then "]" else ")" in
+      bg_str ^ (SingleExp.string_of_single_exp bg) ^ ", " ^ (SingleExp.string_of_single_exp ed) ^ ed_str ^ " step = " ^ (string_of_int step)
+    | TypeVar v -> "TypeVar " ^ (string_of_int v)
+    | TypeTop -> "Top"
+    | TypeBot -> "Bottom"
+    | TypeBExp (op, l, r) -> 
+      let op_str = match op with
+      | TypeAdd -> "Add"
+      | TypeSub -> "Sub"
+      | TypeMul -> "Mul"
+      | TypeSal -> "Sal"
+      | TypeShr -> "Shr"
+      | TypeSar -> "Sar"
+      | TypeXor -> "Xor"
+      | TypeAnd -> "And"
+      | TypeOr -> "Or"
+      | TypeInter -> "Inter"
+      | TypeUnion -> "Union"
+      | TypeDiff -> "Diff"
+      in
+      "BinaryExp (" ^ op_str ^ ", " ^ (string_of_type_exp l) ^ ", " ^ (string_of_type_exp r) ^ ")"
+    | TypeUExp (op, e) ->
+      let op_str = match op with
+      | TypeNot -> "Not"
+      | TypeComp -> "Comp"
+      in
+      "UnaryExp (" ^ op_str ^ ", " ^ (string_of_type_exp e) ^ ")"
+
+  let pp_type_exp (lvl: int) (s: t) =
+    PP.print_lvl lvl "%s" (string_of_type_exp s)
+
+end
