b3dc94f3bc071f62c3be8081897f14adb90b4335
shixins <shixins@mit.edu>
2024-08-16 17:38:48 -0400

    Add an example of using mk_eq.

--- test/test_learn_z3.ml
+++ test/test_learn_z3.ml
@@ -2,7 +2,7 @@ open Z3
 
 let bit_width = 64
 let ctx = mk_context []
-(* let solver = Solver.mk_solver ctx *)
+let solver = Solver.mk_solver ctx None
 
 let x = BitVector.mk_const_s ctx "x" bit_width
 let y = BitVector.mk_const_s ctx "y" bit_width
@@ -13,3 +13,22 @@ let _ = Printf.printf "x = %s\n" (Expr.to_string x)
 let _ = Printf.printf "y = %s\n" (Expr.to_string y)
 let _ = Printf.printf "z = %s\n" (Expr.to_string z)
 let _ = Printf.printf "w = %s\n" (Expr.to_string w)
+
+let a = BitVector.mk_numeral ctx "111" bit_width
+let b = BitVector.mk_numeral ctx "111" bit_width
+let c = BitVector.mk_numeral ctx "222" bit_width
+
+let result_to_string (result: Solver.status) =
+  match result with
+  | Solver.UNKNOWN -> "UNKNOWN"
+  | Solver.SATISFIABLE -> "SATISFIABLE"
+  | Solver.UNSATISFIABLE -> "UNSATISFIABLE"
+
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_eq ctx a b ]))
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_eq ctx a c ]))
+
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_eq ctx x y ]))
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_not ctx (Boolean.mk_eq ctx x y) ]))
+let _ = Solver.add solver [ Boolean.mk_eq ctx x y ]
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_eq ctx x y ]))
+let _ = Printf.printf "%s\n" (result_to_string (Z3.Solver.check solver [ Boolean.mk_not ctx (Boolean.mk_eq ctx x y) ]))
