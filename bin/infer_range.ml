42ee9944be57240992275a41c75d5d8e1296a001
shixins <shixins@mit.edu>
2025-06-11 14:10:52 -0400

    [infer] add comment for infer_range

--- bin/infer_range.ml
+++ bin/infer_range.ml
@@ -10,12 +10,17 @@ let speclist = [
 
 let () =
   Arg.parse speclist (fun _ -> ()) usage_msg;
+
+  (* 1. Parse single infer output *)
   let single_infer_result = 
     Single_type_infer.SingleTypeInfer.state_list_from_file (get_related_filename !program_name "out" "single_infer") 
   in
+
+  (* 2. Parse general interface *)
   let general_interface_list = 
     Func_interface.FuncInterfaceConverter.TaintFuncInterface.interface_list_from_file "./interface/general_func_interface.func_interface" 
   in
+
   let range_infer_result =
     Range_type_infer.RangeTypeInfer.infer single_infer_result general_interface_list
   in
