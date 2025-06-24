e5e557c760e14734771209da83e601c7f95cbc8b
zhizhuoxin <shixins@umich.edu>
2025-06-24 04:32:40 -0400

    [infer] still not correct...

--- lib/single_subtype.ml
+++ lib/single_subtype.ml
@@ -1335,23 +1335,24 @@ module SingleSubtype = struct
           let sub_exp, sub_pc = sub_exp_pc in
           let test_one_sol_template 
               (idx_sol_template: int * SingleExp.t) : 
-              (int * SingleExp.t, int * SingleExp.t) Either.t =
+              (int * SingleExp.t, (int * SingleExp.t) option) Either.t =
             (* if test success, return Left sol_sempate, otherwise, return Right sub_sol !!! *)
             let sol_idx, sol_template = idx_sol_template in
             let sub_context_map = get_context_map sub_pc in
             let sub_sol = SingleExp.repl_var sub_context_map sol_template in
             if sub_exp = SingleTop || sub_sol = SingleTop then 
-              Right (sol_idx, sub_sol) 
+              Right None
             else
               match SingleCondType.check true smt_ctx [Eq, sub_sol, sub_exp] with
               | SatYes -> Left idx_sol_template
-              | _ -> Right (sol_idx, sub_sol)
+              | SatUnknown -> Right (Some (sol_idx, sub_sol))
+              | _ -> Right None
           in
           SmtEmitter.push smt_ctx;
           update_br_context_helper smt_ctx tv_rel_list block_subtype (snd tv_rel.var_idx) sub_pc;
-          let result = List.partition_map test_one_sol_template acc_sol_template_list in
+          let sat_list, unknown_opt_list = List.partition_map test_one_sol_template acc_sol_template_list in
           SmtEmitter.pop smt_ctx 1;
-          result
+          sat_list, List.filter_map (fun x ->  x) unknown_opt_list
         in
         let filter_sub_sat_sol_template
             (acc: (IntSet.t * SingleExpPcSet.t) * ((int * SingleExp.t) list))
@@ -1360,8 +1361,8 @@ module SingleSubtype = struct
           let (acc_checked_var_set, acc_checked_exp_set), acc_sol_template_list = acc in
           if List.is_empty acc_sol_template_list then acc else
           let acc_checked_exp_set = SingleExpPcSet.add sub_exp_pc acc_checked_exp_set in
-          (* unsat_list: fst is in acc_sol_template_list's idx, snd is already in sub_exp_pc's block context *)
-          let sat_list, unsat_list = partition_sub_sat_sol_template acc_sol_template_list sub_exp_pc in
+          (* sat_unknown_list: fst is in acc_sol_template_list's idx, snd is already in sub_exp_pc's block context *)
+          let sat_list, sat_unknown_list = partition_sub_sat_sol_template acc_sol_template_list sub_exp_pc in
           (* if fst tv_rel.var_idx = 338 then
             Printf.printf "@@@var 338\nacc_checked_var_set\n%s\nacc_checked_exp_set\n%s\nacc_sol_template_list\n%s\nsat_list\n%s\n"
             (Sexplib.Sexp.to_string_hum (sexp_of_list sexp_of_int (IntSet.to_list acc_checked_var_set)))
@@ -1371,20 +1372,20 @@ module SingleSubtype = struct
           match sub_exp_pc with
           | SingleVar v, _ -> 
             if IntSet.mem v sup_var_set then
-              let unsat_list = List.filter (fun (_, x) -> x <> SingleExp.SingleTop) unsat_list in
-              if List.is_empty unsat_list then (acc_checked_var_set, acc_checked_exp_set), sat_list else
+              let sat_unknown_list = List.filter (fun (_, x) -> x <> SingleExp.SingleTop) sat_unknown_list in
+              if List.is_empty sat_unknown_list then (acc_checked_var_set, acc_checked_exp_set), sat_list else
               let acc_checked_var_set = IntSet.add v acc_checked_var_set in (* TODO: we may need to check all subtype??? *)
               let sub_tv_rel = List.find (fun (tv_rel: type_rel) -> fst tv_rel.var_idx = v) tv_rel_list in
               let sat_sub_list = 
                 filter_tv_rel_sat_sol_template 
                   (acc_checked_var_set, acc_checked_exp_set) 
-                  (List.split unsat_list |> snd) 
+                  (List.split sat_unknown_list |> snd) 
                   sub_tv_rel 
               in
-              (* sat_sub_list uses idx of unsat_list *)
+              (* sat_sub_list uses idx of sat_unknown_list *)
               let sat_sub_idx_set = List.split sat_sub_list |> fst |> IntSet.of_list in
               let extra_sat_idx_set = 
-                List.filteri (fun i _ -> IntSet.mem i sat_sub_idx_set) unsat_list
+                List.filteri (fun i _ -> IntSet.mem i sat_sub_idx_set) sat_unknown_list
                 |> List.split |> fst |> IntSet.of_list
               in
               let extra_sat_list = List.filter (fun (idx, _) -> IntSet.mem idx extra_sat_idx_set) acc_sol_template_list in
@@ -1392,11 +1393,11 @@ module SingleSubtype = struct
             else (acc_checked_var_set, acc_checked_exp_set), sat_list
           | _ -> (acc_checked_var_set, acc_checked_exp_set), sat_list
         in
-        let filter_todo_subtype (exp_pc: type_pc_t) : bool =
-          match exp_pc with
+        let filter_todo_subtype (exp_pc: type_pc_t) : bool = not (SingleExpPcSet.mem exp_pc (snd checked_subtype_set))
+          (* match exp_pc with
           | SingleVar v, _ ->
             not (IntSet.mem v (fst checked_subtype_set) || SingleExpPcSet.mem exp_pc (snd checked_subtype_set))
-          | _ -> not (SingleExpPcSet.mem exp_pc (snd checked_subtype_set))
+          | _ -> not (SingleExpPcSet.mem exp_pc (snd checked_subtype_set)) *)
           (* we only check exp_pc not in checked_subtype_set *)
         in
         let todo_subtype_list = 
@@ -1417,7 +1418,7 @@ module SingleSubtype = struct
       let direct_subtype_list = get_direct_subtype_list tv_rel in
       let possible_sol_template_list = List.filter_map guess_sol_template direct_subtype_list in
       Printf.printf "###%d\n" (fst tv_rel.var_idx);
-      match filter_tv_rel_sat_sol_template (IntSet.empty, SingleExpPcSet.empty) possible_sol_template_list tv_rel with
+      match filter_tv_rel_sat_sol_template (IntSet.singleton (fst tv_rel.var_idx), SingleExpPcSet.empty) possible_sol_template_list tv_rel with
       | [] -> tv_rel
       | (_, hd) :: _ -> 
         let simp_hd = substitute_one_exp_subtype_list tv_rel_list (hd, snd tv_rel.var_idx) in
